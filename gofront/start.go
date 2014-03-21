016e69efee199859bc05c0a5ace20af2e0b36730
EC2 Default User <ec2-user@ip-172-31-41-215.ec2.internal>
2014-03-21 06:34:36 +0000

    Try still

--- gofront/start.go
+++ gofront/start.go
@@ -28,7 +28,7 @@ var cache_hit_count = 0
 var cache_miss_count = 0
 
 // HBase
-var hbase_conn *goh.HClient
+//var hbase_conn *goh.HClient
 
 var db_type string
 var db_address string
@@ -81,7 +81,7 @@ func query_mysql(resp http.ResponseWriter, req *http.Request) {
 
     fmt.Fprintf(resp, "%s", buffer.String())
 
-  } 
+  }
 }
 
 func query_hbase(resp http.ResponseWriter, req *http.Request) {
@@ -95,22 +95,18 @@ func query_hbase(resp http.ResponseWriter, req *http.Request) {
   tweet_time = strings.Replace(tweet_time, " ", "+", 1)
   row_key := user_id + "|" + tweet_time
   query_count++
-  fmt.Printf("%d Query %s\n", query_count, row_key)
+  //fmt.Printf("%d Query %s\n", query_count, row_key)
+
+  hbase_conn, _ := connect_hbase()
+  defer hbase_conn.Close()
 
   // Query
   data, err := hbase_conn.Get(table, []byte(row_key), "tweet_id", nil)
   if err != nil {
-    fmt.Println("Error in query_hbase :: " + err.Error())
-    return
+    fmt.Printf("(%d) hbase_conn.Get :: %s\n", query_count, err.Error())
+    os.Exit(3)
   }
   if data != nil && len(data) == 1 {
-    /*
-    str := string(data[0].Value)
-    arr := strings.Split(str, ";")
-    arr = arr[0:len(arr)-1]
-    out := strings.Join(arr, "\n") + "\n"
-    buffer.WriteString(out)
-    */
     buffer.WriteString(string(data[0].Value))
   }
 
@@ -149,18 +145,17 @@ func connect_mysql() {
   }
 }
 
-func connect_hbase() {
-  var err error
-
+func connect_hbase() (conn *goh.HClient, err error) {
   address := fmt.Sprintf("%s:9090", db_address)
-  hbase_conn, err = goh.NewTcpClient(address, goh.TBinaryProtocol, false)
-  //defer hbase_conn.Close()
-  if err != nil {
-    panic(err.Error())
+  if conn, err = goh.NewTcpClient(address, goh.TBinaryProtocol, false); err != nil {
+    fmt.Println("NewTcpClient :: " + err.Error())
+    os.Exit(3)
   }
-  if err = hbase_conn.Open(); err != nil {
-    panic(err.Error())
+  if err = conn.Open(); err != nil {
+    fmt.Println("Open :: " + err.Error())
+    os.Exit(3)
   }
+  return conn, err
 }
 
 func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
@@ -179,25 +174,24 @@ func main(){
   }
   db_type = os.Args[1]
   db_address = os.Args[2]
-  fmt.Printf("Connecting to %s database at %s...\n", db_type, db_address)
+  fmt.Printf("%s %s\n", db_type, db_address)
 
   if db_type == "mysql" {
     connect_mysql()
+    defer mysql_conn.Close()
+    fmt.Println("MySQL server connected!")
   } else {
-    connect_hbase()
+    //hbase_conn, _ = connect_hbase()
   }
-  fmt.Println("Database connected!")
-
-  listener, err := net.Listen("tcp","127.0.0.1:9001")
+  listener, err := net.Listen("tcp",":9001")
   if err != nil {
-    fmt.Println(err)
-    return
+    fmt.Println("Listen 127.0.0.1:9001 :: " + err.Error())
+    os.Exit(3)
   }
   cache = make(map[string]string)
   max_cache_size = 10000
   srv := new(FastCGIServer)
 
-  defer mysql_conn.Close()
   fcgi.Serve(listener, srv)
 
 }
