dac92406b61d3c328fcc4303673b6a666339cee4
yoshiaki_ obioka <yosiaki6@gmail.com>
2014-03-26 13:51:28 -0400

    Implement connection pool for mysql

--- gofront/q1_MySQLq2.go
+++ gofront/q1_MySQLq2.go
@@ -16,9 +16,6 @@ import (
 
 type FastCGIServer struct{}
 
-const CACHE_SIZE  = 100000
-const POOL_SIZE  = 900
-var mutex = &sync.Mutex{}
 var db *sql.DB
 var stmtOut *sql.Stmt
 var tweet_id string
@@ -26,10 +23,11 @@ var err error
 var cache map[string] string
 var cache_keys []string
 var delete_cache_key string
-//var hbase_conn_pool [POOL_SIZE]*goh.HClient
+var hbase_conn_pool [POOL_SIZE]*goh.HClient
 var db_conn_pool [POOL_SIZE]*sql.DB
 var stmtOut_pool [POOL_SIZE]*sql.Stmt
-var index_pool []int
+var index_pool [POOL_SIZE]int
+const CACHE_SIZE  = 100000
 
 func (s FastCGIServer) q1(resp http.ResponseWriter, req *http.Request) {
   var buffer bytes.Buffer
@@ -43,7 +41,7 @@ func (s FastCGIServer) q1(resp http.ResponseWriter, req *http.Request) {
 func (s FastCGIServer) q2(resp http.ResponseWriter, req *http.Request) {
   var buffer bytes.Buffer
   buffer.WriteString("GiraffeLovers,3823-5293-0215\n")
-   user_id := req.FormValue("userid")
+   user_id := req.FormValue("user_id")
    tweet_time := req.FormValue("tweet_time")
    tweet_time = strings.Replace( tweet_time, " ", "+",1)
    cache_key := user_id + "_" + tweet_time
@@ -58,12 +56,11 @@ func (s FastCGIServer) q2(resp http.ResponseWriter, req *http.Request) {
   
 
    mutex.Lock()
-   for len(index_pool) == 0 {}
    index := index_pool[0]
    index_pool = index_pool[1:len(index_pool)]
-   mutex.Unlock()
+   mutex.UnLock()
 
-   rows, err := stmtOut_pool[index].Query(user_id, tweet_time)
+   rows, err := stmtOut[index].Query(user_id, tweet_time)
    if err != nil {
      panic(err.Error()) // proper error handling instead of panic in your app
      return
@@ -104,8 +101,8 @@ func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 
 func main(){
   for i := 0; i < POOL_SIZE; i++ {
-    index_pool = append(index_pool, i)
-    db_conn_pool[i], err = sql.Open("mysql", "giraffe:giraffe@tcp(ec2-54-85-197-85.compute-1.amazonaws.com:3306)/cloud")
+    index_pool[i] = i
+    db_conn_pool[i], err = sql.Open("mysql", "root:tobymlab@tcp/cloud")
     if err != nil {
       panic(err.Error())  // Just for example purpose. You should use proper error handling instead of panic
     }
@@ -120,10 +117,8 @@ func main(){
     defer stmtOut_pool[i].Close()
   }
 
-  fmt.Print("Done")
   listener,_:= net.Listen("tcp",":9000")
   cache = make(map[string]string)
   srv := new(FastCGIServer)
   fcgi.Serve(listener, srv)
 }
-
