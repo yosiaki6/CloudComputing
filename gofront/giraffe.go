be55280577dfc85f2878182de7cd5e29016ea6ee
Knott <wittawat@cmu.edu>
2014-04-07 12:45:06 -0700

    Add kirin

--- gofront/giraffe.go
+++ gofront/giraffe.go
@@ -3,18 +3,20 @@ package main
 import (
   "net/http" //package for http based web programs
   "fmt"
+  "log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) { 
-    fmt.Println("Inside handler")
-    // fmt.Fprintf(w, "Hello world from my Go program!")
-    http.Redirect(w, r, "http://localhost:8080/", http.StatusFound)
+  // fmt.Println("Inside handler")
+  user_id := r.FormValue("userid")
+  // tweet_time := r.FormValue("tweet_time")
+  url := fmt.Sprintf("http://ec2-54-84-168-231.compute-1.amazonaws.com:8080/q3phase2/%s/retweeter_id", user_id)
+  http.Redirect(w, r, url, http.StatusFound)
 }
 
 func main() {
-    http.HandleFunc("/q2", handler) // redirect all urls to the handler function
-    // http.Handle("/q2", server)
-    if err := http.ListenAndServe(":80", nil); err != nil {
-        panic(err)
-    }
+  http.HandleFunc("/q3", handler) // redirect all urls to the handler function
+  if err := http.ListenAndServe(":1234", nil); err != nil {
+    log.Fatal(err)
+  }
 }
